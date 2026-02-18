3101a94abef83ffbb5864b6f6d447d307159f0dd
Bob Correa <bob.correa@oclock.school>
2026-02-19 00:07:07 +0100

    maj config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-export const API_URL = 'https://api.craubc.fr/';
-export const API_VERSION = 'v1';
-export const API_ENDPOINT = `${API_URL}/${API_VERSION}`;
-export const API_TIMEOUT = 5000; // Timeout in milliseconds     
-export const STATIC_URL = 'https://craubc.fr/back'; // sans /api
+export const API_URL = 'https://cbc-back2.netlify.app/api';
+export const API_ENDPOINT = API_URL;
+export const API_TIMEOUT = 5000;
